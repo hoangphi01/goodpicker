871b0c6b4992d23dd7752ba9994d7dbdf64fe3a1
hahoang1903 <duchahoang1903@gmail.com>
2021-07-29 08:16:46 +0700

    feat: new-post page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import UserPage from './pages/user/profile'
 import HomePage from './pages/home'
 import LoginPage from './pages/login'
 import AboutUs from './pages/about-us'
+import NewPost from './pages/new-post'
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 						<Route path="/login" component={LoginPage} />
 						<Route path="/profile" component={UserPage} />
 						<Route path="/about-us" component={AboutUs} />
+						<Route path="/new-post" component={NewPost} />
 					</Switch>
 				</div>
 			</div>
