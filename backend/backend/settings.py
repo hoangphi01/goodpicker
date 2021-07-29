fdc16d7db516bc941cd4af460b914f90b707d18e
Hoàng Phi Phạm <hoangphi01@Hoangs-MacBook-Pro.local>
2021-07-29 08:08:32 +0700

    update api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_filters',
     'django_seed',
     'corsheaders',
     'rest_framework',
@@ -132,3 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
